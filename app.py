15a6225b7e35f180bcdf32d7d732e165da42c605
dev global <asnal5665@gmail.com>
2025-01-27 23:15:12 +0530

    "Initial commit: sorting program".

---
+++ app.py
@@ -0,0 +1,12 @@
+# List of colors
+colors = ["Red", "Blue", "Green", "Yellow", "Orange", "Violet", "Indigo"]
+
+# Sort in ascending order
+colors_ascending = sorted(colors)  # Default sorting is ascending
+print("Colors in Ascending Order:", colors_ascending)
+
+# Sort in descending order
+colors_descending = sorted(colors, reverse=True)  # Reverse for descending
+print("Colors in Descending Order:", colors_descending)
+
+
